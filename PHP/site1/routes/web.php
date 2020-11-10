d7490be4bbf09a62d03791250b38da102f57da49
pierre <pierre.dasilva@ynov.com>
2020-11-10 12:48:30 +0100

    Update web.php

--- PHP/site1/routes/web.php
+++ PHP/site1/routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('{n}', function ($n) {
+    return 'Je suis là' . $n . ' ! ';
+})->where("n", "[0-9]+");
+
 Route::get('10', function () {
     return "Je suis à la page 10.";
 });
@@ -28,6 +32,14 @@ Route::get('maview', function () {
 Route::get('mavue/{p1}/{p2}', function ($param1, $param2) {
     return "ma vue nommée" . $param1 . $param2;
 })->name('home2');
+
+Route::get('testjson', function () {
+    return [2, 3 ,4, "lol"];
+});
+
+Route::get('notfound', function () {
+    return response('un test', 418)->header('Content-Type', 'text/plain');
+}); 
 
 Route::get('{n}', function ($n) {
     return "Je suis à la page " . $n . ".";
