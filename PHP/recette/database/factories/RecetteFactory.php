5092427dde324906f01b00bd5aee8894e207f84e
Filtox <pierre-du-12@hotmail.fr>
2020-11-24 15:58:17 +0100

    add

---
+++ PHP/recette/database/factories/RecetteFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Recette;
+use Faker\Generator as Faker;
+
+$factory->define(Recette::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
