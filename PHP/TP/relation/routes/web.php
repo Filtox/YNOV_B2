65200d22a1bb6c3de209e428b5d526703be5a824
Filtox <pierre-du-12@hotmail.fr>
2020-12-28 10:50:16 +0100

    add

---
+++ PHP/TP/relation/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('product/create', 'ProductController@create')->name('product.create');
+Route::get('product/{product}', 'ProductController@show')->name('product.show');
+Route::get('category/product/{product}', 'ProductController@removeCategory')->name('category.product.delete');
